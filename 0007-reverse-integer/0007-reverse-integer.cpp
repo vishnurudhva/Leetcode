84a9c79fb88d01c915853b99dcd73b8f0319b695
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-10-31 01:04:46 +0530

    Time: 3 ms (69.19%), Space: 5.8 MB (75.32%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -7,6 +7,7 @@ public:
             x /= 10;
             if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
             if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
+            cout << rev << " ";
             rev = rev * 10 + pop;
         }
         return rev;
