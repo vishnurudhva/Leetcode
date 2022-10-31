9322ff69155a709c3fef93320b0f792b097e3141
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-10-31 10:12:56 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (95.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if (nums.size() == 1) return nums[0];
         int prev = 0;
         for (int i = 2; i < nums.size(); i++) {
             nums[i] += max(nums[i - 2], prev);
             prev = nums[i - 2];
         }
         
-        return max(nums[nums.size() - 1], nums[nums.size() - 2]);
+        return *max_element(nums.begin(), nums.end());
     }
 };
