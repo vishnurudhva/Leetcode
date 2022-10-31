7cc03ea880c7447dbccd522b46ab48ba329afe7f
Vishnu Rudhva <vishnurudhva01@gmail.com>
2022-10-31 23:25:32 +0530

    Time: 410 ms (29.02%), Space: 98.7 MB (14.97%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -7,9 +7,7 @@ public:
         stk.push(size - 1);
         for (int i = size - 2; i >= 0; i--) {
             while (!stk.empty() && temperatures[stk.top()] <= temperatures[i]) stk.pop();
-            
-            if (!stk.empty())
-                answer[i] = stk.top() - i;
+            if (!stk.empty()) answer[i] = stk.top() - i;
             stk.push(i);
         }
         return answer;
